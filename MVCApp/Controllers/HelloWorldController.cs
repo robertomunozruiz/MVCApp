b8e55e6413b81ca502ddb0176f78afe7e75d7b9a
Roberto <roberto@tiredirect.com.mx>
2023-12-08 00:15:33 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        /*public string Index()
+        {
+           return "This is my default action...";
+        }*/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       /*public string Welcome(string name, int numTimes = 1)
+        {
+            //return "welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, Nuntimes is: {numTimes}");
+        }*/
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ MVCApp/Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCApp.Data;
+using System;
+using System.Linq;
+
+namespace MVCApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCAppContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MVCAppContext>>()))
+            {
+                //Look for any movies
+                if(context.Movie.Any())
+                {
+                    //DB has been seeded
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantinc Comeny",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.9M,
+                        Rating = "pg13"
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
